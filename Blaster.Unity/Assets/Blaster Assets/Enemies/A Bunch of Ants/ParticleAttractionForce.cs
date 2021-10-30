14f95c2eb5d8a0773a8b629bd362a210123add92
Bezhariel <miguelexposito3d@gmail.com>
2021-10-30 10:56:54 +0200

    Ant Explosion Implemented

--- Blaster.Unity/Assets/Blaster Assets/Enemies/A Bunch of Ants/ParticleAttractionForce.cs
+++ Blaster.Unity/Assets/Blaster Assets/Enemies/A Bunch of Ants/ParticleAttractionForce.cs
@@ -10,6 +10,9 @@ public class ParticleAttractionForce : MonoBehaviour
     public Transform target;
     public float speed = 5;
 
+    public float rangeToExplode = 5;
+    public GameObject ExplosionPrefab;
+
     private ParticleSystem pS;
 
     private static ParticleSystem.Particle[] particles; 
@@ -28,14 +31,23 @@ public class ParticleAttractionForce : MonoBehaviour
 
             float distance = Vector3.Distance(target.position, particle.position);
 
-            if (distance > 0.1f)
+            if (distance > rangeToExplode)
             {
                 particle.position = Vector3.Lerp(particle.position, target.position, Time.deltaTime / speed);
                 particles[i] = particle;
+            }
+            else if (distance < rangeToExplode) {
+                SelfDestruction(particles[i].position);
+                particles[i].remainingLifetime = 0;
             }
 
             pS.SetParticles(particles, count);
         
         }
+    }
+
+
+    private void SelfDestruction(Vector3 pos) {
+        Instantiate(ExplosionPrefab, pos, new Quaternion(0,0,0,0));
     }
 }
