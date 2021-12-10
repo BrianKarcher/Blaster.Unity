71b0edc8bce5c172a3bb46d399f6f0c9453385fb
Brian Karcher <bkarcher@colony-games.com>
2021-12-09 20:19:10 -0800

    Ammo updates.

---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/Cinemachine/GetDollySpeed.cs
@@ -0,0 +1,58 @@
+﻿using BlueOrb.Scripts.AI.AtomActions.Animation;
+using HutongGames.PlayMaker;
+using BlueOrb.Common.Container;
+using UnityEngine;
+using Cinemachine;
+using BlueOrb.Messaging;
+using static BlueOrb.Controller.DollyCartComponent;
+using System;
+using BlueOrb.Controller;
+
+namespace BlueOrb.Scripts.AI.Playmaker.Cinemachine
+{
+    [ActionCategory("BlueOrb.Cinemachine")]
+    [HutongGames.PlayMaker.Tooltip("Get speed of dolly cart.")]
+    public class GetDollySpeed : FsmStateAction
+    {
+        [RequiredField]
+        public FsmOwnerDefault gameObject;
+        public FsmFloat Speed;
+        public bool everyFrame;
+
+        //private CinemachineDollyCart _dollyCart;
+
+        //public AddLayerWeightLerpAtom _atom;
+        private DollyCartComponent _dolly;
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+                return;
+            }
+
+            var entity = go.GetComponent<IEntity>();
+            _dolly = entity.Components.GetComponent<DollyCartComponent>();
+            Tick();
+            if (!everyFrame)
+                Finish();
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            Tick();
+        }
+
+        private void Tick()
+        {
+            Speed.Value = _dolly.GetSpeed();
+        }
+    }
+}
--- Blaster.Unity/Assets/Blaster Assets/Scripts/UI/HudController.cs
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/UI/HudController.cs
@@ -7,6 +7,7 @@ using BlueOrb.Messaging;
 //using BlueOrb.Source.UI;
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,193 +17,20 @@ namespace Assets.BlueOrb.Scripts.UI
     public class HudController : ComponentBase<HudController>
     {
         [SerializeField] private Image _secondaryProjectileImage;
+        [SerializeField] private TextMeshProUGUI _secondaryProjectileText;
         [SerializeField] private string _changeProjectileMessage;
+        [SerializeField] private string _setAmmoMessage;
 
         private const string ControllerName = "Hud Controller";
 
         private long _setProjectileId;
+        private long _setAmmoId;
 
         protected override void Awake()
         {
             base.Awake();
             _secondaryProjectileImage.gameObject.SetActive(false);
         }
-
-        //        public GameObject HpBar;
-        //        public GameObject SpBar;
-        //        public GameObject StaminaBar;
-        //        //public GameObject[] ShardGameObjects;
-        //        //private Text[] ShardLabels;
-        //        // TODO Unserialize this when done testing
-        //        //[SerializeField]
-        //        //private ItemConfig[] DisplayedShardConfigs;
-        //        // TODO Unserialize this when done testing
-        //        //[SerializeField]
-        //        //private ItemConfig DisplayedMoldConfig;
-        //        [SerializeField]
-        //        private UIToggleGroup uIToggleGroup;
-        //        //[SerializeField]
-        //        //private float _deadzone = 0.3f;
-        //        //[SerializeField]
-        //        //private string _horizontalAxisName;
-        //        //[SerializeField]
-        //        //private string _verticalAxisName;
-        //        //private List<ShardConfig> ShardConfigs;
-        //        //private List<MoldConfig> MoldConfigs;
-        //        private List<ItemDesc> Shards = new List<ItemDesc>();
-        //        private List<ItemDesc> Molds = new List<ItemDesc>();
-
-        //        private MoldConfig CurrentMold;
-        //        private int _currentMoldIndex;
-        //        //private MoldData MoldData;
-        //        //private Rewired.Player _player;
-        //        //public GameObject MoldHUD;
-        //        //private Image MoldImage;
-        //        [SerializeField]
-        //        private AxisInput _horizontalAxis;
-        //        [SerializeField]
-        //        private AxisInput _verticalAxis;
-
-
-        //        private RectTransform _hpRect;
-        //        private RectTransform _staminaRect;
-
-        //        private Action<Telegram> _setHpDel;
-        //        private Action<Telegram> _setStaminaDel;
-        //        private long _setHpId;
-        //        private bool _axisExtendedLeft = false;
-        //        private bool _axisExtendedRight = false;
-
-        //        protected override void Awake()
-        //        {
-        //            base.Awake();
-        //            _horizontalAxis.Init();
-        //            _verticalAxis.Init();
-        //            uIToggleGroup.SetCount(0);
-        //            _setHpDel = (data) =>
-        //            {
-        //                float val = (float)data.ExtraInfo;
-        //                SetHp(val);
-        //            };
-        //            _setStaminaDel = (data) =>
-        //            {
-        //                float val = (float)data.ExtraInfo;
-        //                SetStamina(val);
-        //            };
-        //            //ShardLabels = new Text[ShardGameObjects.Length];
-        //            //ShardConfigs = new List<ItemConfig>(ShardGameObjects.Length);
-        //            //for (int i = 0; i < ShardGameObjects.Length; i++)
-        //            //{
-        //            //    ShardLabels[i] = ShardGameObjects[i].GetComponent<Text>();
-        //            //}
-        //            //MoldImage = MoldHUD.GetComponent<Image>();
-        //        }
-
-        //        protected void Start()
-        //        {
-        //            //_player = Rewired.ReInput.players.GetPlayer(0);
-        //            _hpRect = HpBar.GetComponent<RectTransform>();
-        //            _staminaRect = StaminaBar.GetComponent<RectTransform>();
-        //        }
-
-        //        public void Update()
-        //        {
-        //            ProcessHorizontalAxis();
-        //            ProcessVerticalAxis();
-        //        }
-
-        //        private void ProcessVerticalAxis()
-        //        {
-        //            if (Molds == null)
-        //                return;
-
-        //            var axisData = _verticalAxis.GetAxisDown();
-        //            bool isChanged = false;
-        //            switch (axisData)
-        //            {
-        //                case AxisInput.AxisExtended.Negative:
-        //                    //uIToggleGroup.Toggle(false);
-        //                    _currentMoldIndex--;
-        //                    isChanged = true;
-        //                    break;
-        //                case AxisInput.AxisExtended.Positive:
-        //                    //uIToggleGroup.Toggle(true);
-        //                    _currentMoldIndex++;
-        //                    isChanged = true;
-        //                    break;
-        //            }
-        //            if (!isChanged)
-        //                return;
-
-        //            ProcessMoldBoundsCheck();
-
-        //            CurrentMold = Molds[_currentMoldIndex].ItemConfig as MoldConfig;
-        //            GameStateController.Instance.CurrentMold = CurrentMold;
-        //            DisplayMoldData();
-        //        }
-
-        //        private void ProcessMoldBoundsCheck()
-        //        {
-        //            // Bounds check
-        //            if (_currentMoldIndex >= Molds.Count)
-        //            {
-        //                _currentMoldIndex = 0;
-        //            }
-        //            if (_currentMoldIndex < 0)
-        //            {
-        //                _currentMoldIndex = Molds.Count - 1;
-        //            }
-        //        }
-
-        //        private void ProcessHorizontalAxis()
-        //        {
-        //            var axisData = _horizontalAxis.GetAxisDown();
-        //            bool isChanged = false;
-        //            switch (axisData)
-        //            {
-        //                case AxisInput.AxisExtended.Negative:
-        //                    uIToggleGroup.Toggle(false);
-        //                    isChanged = true;
-        //                    break;
-        //                case AxisInput.AxisExtended.Positive:
-        //                    uIToggleGroup.Toggle(true);
-        //                    isChanged = true;
-        //                    break;
-        //            }
-        //            if (isChanged)
-        //            {
-        //                var currentShard = uIToggleGroup.GetCurrentItem().GetItemConfig();
-        //                // The "as" will make MoldConfigs null automatically.
-        //                GameStateController.Instance.CurrentShard = currentShard as ShardConfig;
-        //            }
-        //            //var currentAxis = _player.GetAxis(_horizontalAxisName);
-
-        //            //if (currentAxis < -_deadzone && !_axisExtendedLeft)
-        //            //{
-        //            //    uIToggleGroup.Toggle(false);
-        //            //    //newItem = ToggleDirection == ToggleDirection.Right ? CurrentItem - 1 : CurrentItem + 1;
-        //            //    //SetCurrentItem(newItem);
-        //            //    _axisExtendedLeft = true;
-        //            //    _axisExtendedRight = false;
-        //            //}
-        //            //if (currentAxis > -_deadzone && currentAxis < _deadzone)
-        //            //{
-        //            //    _axisExtendedLeft = false;
-        //            //    _axisExtendedRight = false;
-        //            //}
-        //            //if (currentAxis > _deadzone && !_axisExtendedRight)
-        //            //{
-        //            //    uIToggleGroup.Toggle(true);
-        //            //    //newItem = ToggleDirection == ToggleDirection.Right ? CurrentItem + 1 : CurrentItem - 1;
-        //            //    //SetCurrentItem(newItem);
-        //            //    _axisExtendedLeft = false;
-        //            //    _axisExtendedRight = true;
-        //            //}
-
-        //            // Is it a mold?
-        //            //if (currentShard.ItemType != ItemTypeEnum.Shard)
-        //            //    currentShard = null;
-        //        }
 
         public override void StartListening()
         {
@@ -211,11 +39,25 @@ namespace Assets.BlueOrb.Scripts.UI
 
             _setProjectileId = MessageDispatcher.Instance.StartListening(_changeProjectileMessage, ControllerName, (data) =>
             {
-                _secondaryProjectileImage.gameObject.SetActive(true);
-                var projectileConfig = data.ExtraInfo as ProjectileConfig;
-                _secondaryProjectileImage.sprite = projectileConfig.HUDImageSelected;
+                if (data.ExtraInfo == null)
+                {
+                    _secondaryProjectileImage.gameObject.SetActive(false);
+                    _secondaryProjectileImage.sprite = null;
+                    _secondaryProjectileText.text = null;
+                }
+                else
+                {
+                    _secondaryProjectileImage.gameObject.SetActive(true);
+                    var projectileConfig = data.ExtraInfo as ProjectileConfig;
+                    _secondaryProjectileImage.sprite = projectileConfig.HUDImageSelected;
+                    _secondaryProjectileText.text = projectileConfig.Ammo.ToString();
+                }
+            });
 
-
+            _setAmmoId = MessageDispatcher.Instance.StartListening(_setAmmoMessage, ControllerName, (data) =>
+            {
+                var ammo = (int)data.ExtraInfo;
+                _secondaryProjectileText.text = ammo.ToString();
             });
 
             //            MessageDispatcher.Instance.StartListening("SetShardCount", "Hud Controller", (data) =>
@@ -287,6 +129,7 @@ namespace Assets.BlueOrb.Scripts.UI
         {
             base.StopListening();
             MessageDispatcher.Instance.StopListening(_changeProjectileMessage, ControllerName, _setProjectileId);
+            MessageDispatcher.Instance.StopListening(_setAmmoMessage, ControllerName, _setAmmoId);
         }
 
         //        private void DisplayMoldData()
@@ -337,4 +180,182 @@ namespace Assets.BlueOrb.Scripts.UI
         //            _staminaRect.localScale = new Vector3(value, 1, 1);
         //        }
     }
+
+    //        public GameObject HpBar;
+    //        public GameObject SpBar;
+    //        public GameObject StaminaBar;
+    //        //public GameObject[] ShardGameObjects;
+    //        //private Text[] ShardLabels;
+    //        // TODO Unserialize this when done testing
+    //        //[SerializeField]
+    //        //private ItemConfig[] DisplayedShardConfigs;
+    //        // TODO Unserialize this when done testing
+    //        //[SerializeField]
+    //        //private ItemConfig DisplayedMoldConfig;
+    //        [SerializeField]
+    //        private UIToggleGroup uIToggleGroup;
+    //        //[SerializeField]
+    //        //private float _deadzone = 0.3f;
+    //        //[SerializeField]
+    //        //private string _horizontalAxisName;
+    //        //[SerializeField]
+    //        //private string _verticalAxisName;
+    //        //private List<ShardConfig> ShardConfigs;
+    //        //private List<MoldConfig> MoldConfigs;
+    //        private List<ItemDesc> Shards = new List<ItemDesc>();
+    //        private List<ItemDesc> Molds = new List<ItemDesc>();
+
+    //        private MoldConfig CurrentMold;
+    //        private int _currentMoldIndex;
+    //        //private MoldData MoldData;
+    //        //private Rewired.Player _player;
+    //        //public GameObject MoldHUD;
+    //        //private Image MoldImage;
+    //        [SerializeField]
+    //        private AxisInput _horizontalAxis;
+    //        [SerializeField]
+    //        private AxisInput _verticalAxis;
+
+
+    //        private RectTransform _hpRect;
+    //        private RectTransform _staminaRect;
+
+    //        private Action<Telegram> _setHpDel;
+    //        private Action<Telegram> _setStaminaDel;
+    //        private long _setHpId;
+    //        private bool _axisExtendedLeft = false;
+    //        private bool _axisExtendedRight = false;
+
+    //        protected override void Awake()
+    //        {
+    //            base.Awake();
+    //            _horizontalAxis.Init();
+    //            _verticalAxis.Init();
+    //            uIToggleGroup.SetCount(0);
+    //            _setHpDel = (data) =>
+    //            {
+    //                float val = (float)data.ExtraInfo;
+    //                SetHp(val);
+    //            };
+    //            _setStaminaDel = (data) =>
+    //            {
+    //                float val = (float)data.ExtraInfo;
+    //                SetStamina(val);
+    //            };
+    //            //ShardLabels = new Text[ShardGameObjects.Length];
+    //            //ShardConfigs = new List<ItemConfig>(ShardGameObjects.Length);
+    //            //for (int i = 0; i < ShardGameObjects.Length; i++)
+    //            //{
+    //            //    ShardLabels[i] = ShardGameObjects[i].GetComponent<Text>();
+    //            //}
+    //            //MoldImage = MoldHUD.GetComponent<Image>();
+    //        }
+
+    //        protected void Start()
+    //        {
+    //            //_player = Rewired.ReInput.players.GetPlayer(0);
+    //            _hpRect = HpBar.GetComponent<RectTransform>();
+    //            _staminaRect = StaminaBar.GetComponent<RectTransform>();
+    //        }
+
+    //        public void Update()
+    //        {
+    //            ProcessHorizontalAxis();
+    //            ProcessVerticalAxis();
+    //        }
+
+    //        private void ProcessVerticalAxis()
+    //        {
+    //            if (Molds == null)
+    //                return;
+
+    //            var axisData = _verticalAxis.GetAxisDown();
+    //            bool isChanged = false;
+    //            switch (axisData)
+    //            {
+    //                case AxisInput.AxisExtended.Negative:
+    //                    //uIToggleGroup.Toggle(false);
+    //                    _currentMoldIndex--;
+    //                    isChanged = true;
+    //                    break;
+    //                case AxisInput.AxisExtended.Positive:
+    //                    //uIToggleGroup.Toggle(true);
+    //                    _currentMoldIndex++;
+    //                    isChanged = true;
+    //                    break;
+    //            }
+    //            if (!isChanged)
+    //                return;
+
+    //            ProcessMoldBoundsCheck();
+
+    //            CurrentMold = Molds[_currentMoldIndex].ItemConfig as MoldConfig;
+    //            GameStateController.Instance.CurrentMold = CurrentMold;
+    //            DisplayMoldData();
+    //        }
+
+    //        private void ProcessMoldBoundsCheck()
+    //        {
+    //            // Bounds check
+    //            if (_currentMoldIndex >= Molds.Count)
+    //            {
+    //                _currentMoldIndex = 0;
+    //            }
+    //            if (_currentMoldIndex < 0)
+    //            {
+    //                _currentMoldIndex = Molds.Count - 1;
+    //            }
+    //        }
+
+    //        private void ProcessHorizontalAxis()
+    //        {
+    //            var axisData = _horizontalAxis.GetAxisDown();
+    //            bool isChanged = false;
+    //            switch (axisData)
+    //            {
+    //                case AxisInput.AxisExtended.Negative:
+    //                    uIToggleGroup.Toggle(false);
+    //                    isChanged = true;
+    //                    break;
+    //                case AxisInput.AxisExtended.Positive:
+    //                    uIToggleGroup.Toggle(true);
+    //                    isChanged = true;
+    //                    break;
+    //            }
+    //            if (isChanged)
+    //            {
+    //                var currentShard = uIToggleGroup.GetCurrentItem().GetItemConfig();
+    //                // The "as" will make MoldConfigs null automatically.
+    //                GameStateController.Instance.CurrentShard = currentShard as ShardConfig;
+    //            }
+    //            //var currentAxis = _player.GetAxis(_horizontalAxisName);
+
+    //            //if (currentAxis < -_deadzone && !_axisExtendedLeft)
+    //            //{
+    //            //    uIToggleGroup.Toggle(false);
+    //            //    //newItem = ToggleDirection == ToggleDirection.Right ? CurrentItem - 1 : CurrentItem + 1;
+    //            //    //SetCurrentItem(newItem);
+    //            //    _axisExtendedLeft = true;
+    //            //    _axisExtendedRight = false;
+    //            //}
+    //            //if (currentAxis > -_deadzone && currentAxis < _deadzone)
+    //            //{
+    //            //    _axisExtendedLeft = false;
+    //            //    _axisExtendedRight = false;
+    //            //}
+    //            //if (currentAxis > _deadzone && !_axisExtendedRight)
+    //            //{
+    //            //    uIToggleGroup.Toggle(true);
+    //            //    //newItem = ToggleDirection == ToggleDirection.Right ? CurrentItem + 1 : CurrentItem - 1;
+    //            //    //SetCurrentItem(newItem);
+    //            //    _axisExtendedLeft = false;
+    //            //    _axisExtendedRight = true;
+    //            //}
+
+    //            // Is it a mold?
+    //            //if (currentShard.ItemType != ItemTypeEnum.Shard)
+    //            //    currentShard = null;
+    //        }
+
+
 }
