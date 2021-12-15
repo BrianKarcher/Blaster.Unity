8bcb347a62ab2a4143a04f074571a1e35ee6bd06
Brian Karcher <bkarcher@colony-games.com>
2021-12-15 00:10:30 -0800

    Manual movement updates.

--- Blaster.Unity/Assets/Blaster Assets/Scripts/AI/AtomActions/Player/OverShoulderMovementAtom.cs
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/AtomActions/Player/OverShoulderMovementAtom.cs
@@ -12,16 +12,13 @@
 //    {
 //        private ThirdPersonUserControl _thirdPersonUserControl;
 //        private PlayerController _playerController;
-//        private Animator _animator;
-//        private PhysicsComponent _physicsComponent;
-//        private AnimationComponent _animationComponent;
+
 //        //private ThirdPersonCameraController _camera;
 //        //private PlayerController _playerController;
 //        //private Vector3 _axisInput;
 //        //private float _forwardAmount;
 //        //private float _turnAmount;
 //        //private PhysicsComponent _physicsComponent;
-//        //protected Animator _animator;
 
 //        //private OverShoulderMovementLogic _overShoulderLogic;
 
@@ -30,20 +27,15 @@
 //            base.Start(entity);
 //            if (_playerController == null)            
 //                _playerController = entity.Components.GetComponent<PlayerController>();
-            
-//            if (_animator == null)         
-//                _animator = entity.transform.GetComponent<Animator>();
-            
+
 //            //if (_overShoulderLogic == null)
 //            //{
 //            //    _overShoulderLogic = new OverShoulderMovementLogic(_playerController);
 //            //}
 //            if (_thirdPersonUserControl == null)
 //                _thirdPersonUserControl = entity.Components.GetComponent<ThirdPersonUserControl>();
-//            if (_physicsComponent == null)
-//                _physicsComponent = entity.Components.GetComponent<PhysicsComponent>();
-//            if (_animationComponent == null)
-//                _animationComponent = entity.Components.GetComponent<AnimationComponent>();
+
+
 //            //_playerController.SetMovementActive(true);
 //            _playerController.SetSkillMovementLogic(PlayerController.MovementLogic.OverShoulder);
 //            _animator.SetBool(_playerController.StrafeAnim, true);
@@ -70,39 +62,6 @@
 
 //            var _movementData = _thirdPersonUserControl.GetInputData();
 //            ProcessInput(_movementData);
-//        }
-
-//        public void ProcessInput(MovementInputData inputData)
-//        {
-//            if (inputData.DirectionalInput_Raw.magnitude > 1f) inputData.DirectionalInput_Raw.Normalize();
-
-//            var axisInput = inputData.DirectionalInput_Raw;
-//            if (axisInput.sqrMagnitude > 0.1f)
-//            {
-//                int i = 1;
-//            }
-
-//            // _forwardAmount = axisInput.magnitude;
-//            var forwardAmount = axisInput.y;
-//            var _sideAmount = axisInput.x;
-//            float _turnAmount = 0f;
-
-//            //_playerController.UpdateAnimator(_forwardAmount, _turnAmount, _sideAmount);
-//            _animationComponent.SetForwardSpeed(forwardAmount);
-//            _animationComponent.SetTurnSpeed(_turnAmount);
-//            _animationComponent.SetSideSpeed(_sideAmount);
-//            if (_physicsComponent.Controller.GetIsGrounded())
-//                _animationComponent.SetVerticalSpeed(0f);
-//            else
-//                _animationComponent.SetVerticalSpeed(_physicsComponent.GetVelocity3().y);
-
-//            var playerRelativeInputDirection = _playerController.transform.TransformDirection(axisInput.xz());
-//            _playerController.SetTargetVelocity(playerRelativeInputDirection * _physicsComponent.GetPhysicsData().MaxSpeed);
-//            if (inputData.TurnCrouchOn)
-//                _playerController.SetCrouch(true);
-//            if (inputData.TurnCrouchOff)
-//                _playerController.SetCrouch(false);
-
 //        }
 
 //        public override void End()
--- Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/Player/OverShoulderMovement.cs
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/Player/OverShoulderMovement.cs
@@ -1,53 +1,121 @@
-﻿//using BlueOrb.Scripts.AI.AtomActions;
-//using BlueOrb.Scripts.AI.AtomActions.Player;
-//using HutongGames.PlayMaker;
-//using BlueOrb.Common.Container;
+﻿using BlueOrb.Scripts.AI.AtomActions;
+using HutongGames.PlayMaker;
+using BlueOrb.Common.Container;
+using UnityEngine;
+using BlueOrb.Physics;
+using BlueOrb.Messaging;
+using BlueOrb.Controller.Player;
+using BlueOrb.Controller;
 
-//namespace BlueOrb.Scripts.AI.PlayMaker.Attack
-//{
-//    [ActionCategory("RQ.Player")]
-//    //[Tooltip("Returns Success if Button is pressed.")]
-//    public class OverShoulderMovement : FsmStateAction
-//    {
-//        [RequiredField]
-//        public FsmOwnerDefault gameObject;
-//        public OverShoulderMovementAtom _atom;
+namespace BlueOrb.Scripts.AI.PlayMaker.Attack
+{
+    [ActionCategory("BlueOrb.Player")]
+    //[Tooltip("Returns Success if Button is pressed.")]
+    public class OverShoulderMovement : FsmStateAction
+    {
+        [RequiredField]
+        public FsmOwnerDefault gameObject;
+        public string InputMessage;
+        private Animator _animator;
+        private PhysicsComponent _physicsComponent;
+        private AnimationComponent _animationComponent;
+        private long _inputMessageIndex;
+        private IEntity _entity;
 
-//        //[HutongGames.PlayMaker.Tooltip("Event to send if the trigger event is detected.")]
-//        //public FsmEvent HangEvent;
+        //        public OverShoulderMovementAtom _atom;
 
-//        public override void Reset()
-//        {
-//            gameObject = null;
-//        }
+        //        //[HutongGames.PlayMaker.Tooltip("Event to send if the trigger event is detected.")]
+        //        //public FsmEvent HangEvent;
 
-//        public override void OnEnter()
-//        {
-//            var go = Fsm.GetOwnerDefaultTarget(gameObject);
-//            if (go == null)
-//            {
-//                return;
-//            }
-//            var entity = go.GetComponent<IEntity>();
-//            _atom.Start(entity);
-//        }
+        public override void Reset()
+        {
+            gameObject = null;
+        }
 
-//        public override void OnExit()
-//        {
-//            base.OnExit();
-//            _atom.End();
-//        }
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+                return;
+            }
+            _entity = go.GetComponent<IEntity>();
+            if (_animator == null)
+                _animator = _entity.transform.GetComponent<Animator>();
+            if (_physicsComponent == null)
+                _physicsComponent = _entity.Components.GetComponent<PhysicsComponent>();
+            if (_animationComponent == null)
+                _animationComponent = _entity.Components.GetComponent<AnimationComponent>();
+            //_atom.Start(entity);
+            StartListening();
+        }
 
-//        public override void OnUpdate()
-//        {
-//            Tick();
-//        }
+        public void StartListening()
+        {
+            _inputMessageIndex = MessageDispatcher.Instance.StartListening(InputMessage, _entity.GetId(), (data) =>
+            {
+                var inputData = (MovementInputData)data.ExtraInfo;
+                ProcessInput(inputData);
+            });
+        }
 
-//        private void Tick()
-//        {
-//            _atom.OnUpdate();
-//            if (_atom.IsFinished)
-//                Finish();
-//        }
-//    }
-//}
+        public void ProcessInput(MovementInputData inputData)
+        {
+            if (inputData.DirectionalInput_Raw.magnitude > 1f) inputData.DirectionalInput_Raw.Normalize();
+
+            var axisInput = inputData.DirectionalInput_Raw;
+            if (axisInput.sqrMagnitude > 0.1f)
+            {
+                int i = 1;
+            }
+
+            // _forwardAmount = axisInput.magnitude;
+            var forwardAmount = axisInput.y;
+            var _sideAmount = axisInput.x;
+            float _turnAmount = 0f;
+
+            //_playerController.UpdateAnimator(_forwardAmount, _turnAmount, _sideAmount);
+            _animationComponent?.SetForwardSpeed(forwardAmount);
+            _animationComponent?.SetTurnSpeed(_turnAmount);
+            _animationComponent?.SetSideSpeed(_sideAmount);
+            if (_physicsComponent.Controller.GetIsGrounded())
+                _animationComponent?.SetVerticalSpeed(0f);
+            else
+                _animationComponent?.SetVerticalSpeed(_physicsComponent.GetVelocity3().y);
+
+            //var playerRelativeInputDirection = _playerController.transform.TransformDirection(axisInput.xz());
+            var playerRelativeInputDirection = _entity.transform.TransformDirection(new Vector3(axisInput.x, 0, axisInput.y));
+            _physicsComponent.SetVelocity3(playerRelativeInputDirection * _physicsComponent.GetPhysicsData().MaxSpeed);
+            //_playerController.SetTargetVelocity(playerRelativeInputDirection * _physicsComponent.GetPhysicsData().MaxSpeed);
+            //if (inputData.TurnCrouchOn)
+            //    _playerController.SetCrouch(true);
+            //if (inputData.TurnCrouchOff)
+            //    _playerController.SetCrouch(false);
+
+        }
+
+        private void StopListening(IEntity entity)
+        {
+            MessageDispatcher.Instance.StopListening(InputMessage, entity.GetId(), _inputMessageIndex);
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+            //_atom.End();
+            StopListening(_entity);
+        }
+
+        //        public override void OnUpdate()
+        //        {
+        //            Tick();
+        //        }
+
+        //        private void Tick()
+        //        {
+        //            _atom.OnUpdate();
+        //            if (_atom.IsFinished)
+        //                Finish();
+        //        }
+    }
+}
