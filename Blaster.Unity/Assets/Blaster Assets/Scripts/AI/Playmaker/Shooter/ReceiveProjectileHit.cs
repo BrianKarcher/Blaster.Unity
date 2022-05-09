efd763872b87ba6c7d29ee8f59b95d0ad1a32700
Brian Karcher <bkarcher@colony-games.com>
2022-05-09 00:24:27 -0700

    Shield Hit updates.

---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/Shooter/ReceiveProjectileHit.cs
@@ -0,0 +1,61 @@
+﻿using HutongGames.PlayMaker;
+using BlueOrb.Common.Container;
+using BlueOrb.Messaging;
+using UnityEngine;
+using BlueOrb.Scripts.AI.Playmaker;
+
+namespace BlueOrb.Scripts.AI.PlayMaker.Attack
+{
+    [ActionCategory("BlueOrb.Shooter")]
+    public class ReceiveProjectileHit : BasePlayMakerAction
+    {
+        [RequiredField]
+        public FsmOwnerDefault gameObject;
+        public FsmString Message;
+        [UIHint(UIHint.Tag)]
+        public FsmString ShieldTag;
+
+        public FsmEvent Hit;
+        public FsmEvent ShieldHit;
+        private IEntity entity;
+        private long messageId;
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+                return;
+            }
+            this.entity = base.GetEntityBase(go);
+            StartListening(entity);
+        }
+
+        private void StartListening(IEntity entity)
+        {
+            this.messageId = MessageDispatcher.Instance.StartListening(Message.Value, entity.GetId(), (data) =>
+            {
+                GameObject collider = data.ExtraInfo as GameObject;
+                if (collider != null && collider.CompareTag(ShieldTag.Value))
+                {
+                    Fsm.Event(ShieldHit);
+                }
+                else
+                {
+                    Fsm.Event(Hit);
+                }
+            });
+        }
+
+        public override void OnExit()
+        {
+            StopListening(this.entity);
+            base.OnExit();
+        }
+
+        private void StopListening(IEntity entity)
+        {
+            MessageDispatcher.Instance.StopListening(Message.Value, entity.GetId(), this.messageId);
+        }
+    }
+}
---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/Shooter/SendProjectileHit.cs
@@ -0,0 +1,30 @@
+﻿using HutongGames.PlayMaker;
+using BlueOrb.Messaging;
+using UnityEngine;
+using BlueOrb.Scripts.AI.Playmaker;
+
+namespace BlueOrb.Scripts.AI.PlayMaker.Attack
+{
+    [ActionCategory("BlueOrb.Shooter")]
+    public class SendProjectileHit : BasePlayMakerAction
+    {
+        [RequiredField]
+        public FsmOwnerDefault gameObject;
+        public FsmString Message;
+        public FsmGameObject Receiver;
+        public FsmGameObject Collider;
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+                return;
+            }
+            var entity = base.GetEntityBase(go);
+            var receiverEntity = base.GetEntityBase(Receiver.Value);
+            MessageDispatcher.Instance.DispatchMsg(Message.Value, 0f, entity.GetId(), receiverEntity.GetId(), Collider.Value);
+            Finish();
+        }
+    }
+}
