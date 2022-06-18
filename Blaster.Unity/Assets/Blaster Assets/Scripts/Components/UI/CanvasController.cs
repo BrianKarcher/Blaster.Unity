0d6e7d76f6c0bfd8f884fdbc84eabe4395e95d5b
Brian Karcher <bkarcher@colony-games.com>
2022-06-18 04:11:21 -0700

    UI updates. Default button updates. Mouse click on background updates.

--- Blaster.Unity/Assets/Blaster Assets/Scripts/Components/UI/CanvasController.cs
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/Components/UI/CanvasController.cs
@@ -12,22 +12,52 @@ namespace BlueOrb.Controller.Inventory
         private RewiredEventSystem rewiredEventSystem;
         [SerializeField]
         private Selectable firstSelectedGameObject;
+        private RewiredStandaloneInputModule inputModule;
+
+        private Rewired.Player _player;
 
         public override void OnEnable()
         {
             base.OnEnable();
+            _player = Rewired.ReInput.players.GetPlayer(0);
+            if (inputModule == null)
+            {
+                inputModule = GameObject.FindObjectOfType<RewiredStandaloneInputModule>();
+            }
+            rewiredEventSystem.SetSelectedGameObject(null);
+            //rewiredEventSystem.
+            //rewiredEventSystem?.SetSelectedGameObject(firstSelectedGameObject.gameObject);
+
+        }
+
+        public void Update()
+        {
+            if (rewiredEventSystem.currentSelectedGameObject == null)
+            {
+                var currentHAxis = _player.GetAxis(inputModule.horizontalAxis);
+                var currentVAxis = _player.GetAxis(inputModule.verticalAxis);
+                if (Mathf.Abs(currentHAxis) > 0.3f || Mathf.Abs(currentVAxis) > 0.3f)
+                {
+                    rewiredEventSystem?.SetSelectedGameObject(firstSelectedGameObject.gameObject);
+                }
+            }
+        }
+
+        public void Start()
+        {
+            //base.start();
             //Debug.Log($"Setting selected object to {firstSelectedGameObject.name}");
             //firstSelectedGameObject.Select();
-            rewiredEventSystem?.SetSelectedGameObject(firstSelectedGameObject.gameObject);
+            //rewiredEventSystem?.SetSelectedGameObject(firstSelectedGameObject.gameObject);
         }
 
         public override void OnDisable()
         {
             base.OnDisable();
-            if (rewiredEventSystem.currentSelectedGameObject != null)
-            {
-                this.firstSelectedGameObject = rewiredEventSystem.currentSelectedGameObject.GetComponent<Selectable>();
-            }
+            //if (rewiredEventSystem.currentSelectedGameObject != null)
+            //{
+            //    this.firstSelectedGameObject = rewiredEventSystem.currentSelectedGameObject.GetComponent<Selectable>();
+            //}
         }
     }
 }
