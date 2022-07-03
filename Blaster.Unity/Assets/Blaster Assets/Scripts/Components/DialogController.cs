7ab388117e8ddc1b25b1ce1be85e4776e0af3a13
Brian Karcher <bkarcher@colony-games.com>
2022-07-02 22:18:34 -0700

    Dialog updates.

---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/Components/DialogController.cs
@@ -0,0 +1,20 @@
+﻿//using BlueOrb.Common.Components;
+//using UnityEngine;
+
+//namespace BlueOrb.Controller.Inventory
+//{
+//    [AddComponentMenu("BlueOrb/Components/Dialog Controller")]
+//    public class DialogController : ComponentBase<DialogController>
+//    {
+//        public void ConversationStart()
+//        {
+//            Debug.LogError("Conversation Started");
+
+//        }
+
+//        public void ConversationEnd()
+//        {
+//            Debug.LogError("Conversation Ended");
+//        }
+//    }
+//}
