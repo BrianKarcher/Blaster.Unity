dff0351b9ed95914db97ad52cefa5da50b65901b
Brian Karcher <bkarcher@colony-games.com>
2022-07-11 01:52:40 -0700

    High score persistence updates.

---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/Manager/ProcessEndStage.cs
@@ -0,0 +1,16 @@
+﻿using HutongGames.PlayMaker;
+using BlueOrb.Common.Container;
+
+namespace BlueOrb.Scripts.AI.Playmaker.Manager
+{
+    [ActionCategory("BlueOrb.Manager")]
+    [HutongGames.PlayMaker.Tooltip("End stage processing")]
+    public class ProcessEndStage : BasePlayMakerAction
+    {
+        public override void OnEnter()
+        {
+            EntityContainer.Instance.LevelStateController.ProcessEndStage();
+            Finish();
+        }
+    }
+}
