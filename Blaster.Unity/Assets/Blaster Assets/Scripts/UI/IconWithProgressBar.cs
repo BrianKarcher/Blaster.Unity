4c9c172ae62fe21f2342f4feaaa88b643e39c7ce
Brian Karcher <bkarcher@colony-games.com>
2022-07-25 23:55:32 -0700

    Progress Bar UI control.

---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/UI/IconWithProgressBar.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Blaster_Assets.Scripts.UI
+{
+    public class IconWithProgressBar : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject bar;
+        [SerializeField]
+        private Image image;
+
+        //[SerializeField]
+        //private TextMeshProUGUI textMeshProUGUI;
+        //private string TweenName => $"{this.GetInstanceID()}_valuetween";
+        private float scale = 1f;
+
+        public void SetImage(Sprite image) => this.image.sprite = image;
+
+        public void SetValue(float scale)
+        {
+            this.scale = scale;
+            SetScrollbarScale(this.scale);
+            //int.TryParse(_textMeshProUGUI.text, out int currentScore);
+            //iTween.ScaleTo
+            //iTween.ValueTo(gameObject, iTween.Hash("name", TweenName, "from", currentScore, "to", score, "time", 1, "onupdate", "UpdateScore"));
+        }
+
+        private void SetScrollbarScale(float scale) => bar.transform.localScale = new Vector3(scale, 1, 1);
+
+        //public void SetScoreImmediate(int score)
+        //{
+        //    iTween.StopByName(TweenName);
+        //    UpdateScore(score);
+        //}
+    }
+}
