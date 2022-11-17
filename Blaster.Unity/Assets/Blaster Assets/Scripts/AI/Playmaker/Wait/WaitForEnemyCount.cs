55632563e173801d7bdaa1c93c2baf506f7d860f
Brian Karcher <bkarcher@colony-games.com>
2022-11-16 23:09:55 -0800

    Enemy Limiter.

---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/Wait/WaitForEnemyCount.cs
@@ -0,0 +1,48 @@
+﻿using HutongGames.PlayMaker;
+using BlueOrb.Common.Container;
+
+namespace BlueOrb.Scripts.AI.Playmaker.Input
+{
+    [ActionCategory("BlueOrb")]
+    [HutongGames.PlayMaker.Tooltip("Wait until an active enemy count has been reached")]
+    public class WaitForEnemyCount : BasePlayMakerAction
+    {
+        [RequiredField]
+        public FsmOwnerDefault gameObject;
+
+        [RequiredField]
+        public FsmInt EnemyCount = 6;
+
+        public override void Reset()
+        {
+            gameObject = null;
+            EnemyCount = 6;
+        }
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+                return;
+            }
+
+            var entity = base.GetEntityBase(go);
+            if (CheckComplete())
+            {
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (CheckComplete())
+            {
+                Finish();
+            }
+        }
+
+        private bool CheckComplete()
+            => EntityContainer.Instance.GetEnemies().Count <= this.EnemyCount.Value;
+    }
+}
