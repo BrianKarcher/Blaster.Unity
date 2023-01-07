7be1ed6cfea13cafc505ec04e8b8c1f7207a8fc6
Brian Karcher <bkarcher@colony-games.com>
2023-01-07 00:59:57 -0800

    Full boss loop! He can be killed!!!!!!!!!!!!!!!

---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/Stats/IsDead.cs
@@ -0,0 +1,56 @@
+﻿using HutongGames.PlayMaker;
+using BlueOrb.Common.Container;
+using BlueOrb.Controller.Damage;
+
+namespace BlueOrb.Scripts.AI.Playmaker.Enemies
+{
+    [ActionCategory("BlueOrb")]
+    [HutongGames.PlayMaker.Tooltip("Is Dead State")]
+    public class IsDead : BasePlayMakerAction
+    {
+        [RequiredField]
+        public FsmOwnerDefault gameObject;
+
+        [RequiredField]
+        public FsmEvent BossDeadEvent;
+        private IEntity entity;
+        private EntityStatsComponent entityStatsComponent;
+        public bool everyFrame;
+
+        public override void Reset()
+        {
+            gameObject = null;
+        }
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+                return;
+            }
+            this.entity = base.GetEntityBase(go);
+            if (entityStatsComponent == null)
+            {
+                this.entityStatsComponent = this.entity.GetComponent<EntityStatsComponent>();
+            }
+            Tick();
+            if (!everyFrame)
+                Finish();
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            Tick();
+        }
+
+        private void Tick()
+        {
+            if (this.entityStatsComponent.GetEntityStats().CurrentHP <= 0)
+            {
+                Fsm.Event(BossDeadEvent);
+            }
+        }
+    }
+}
