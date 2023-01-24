a1eecdfee2157e3dd5013dc91ec2ef61e3d5e42f
Brian Karcher <bkarcher@colony-games.com>
2023-01-23 23:51:08 -0800

    Sound effects. Kamikaze alert message. Alert Action.

---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/UI/Alert.cs
@@ -0,0 +1,36 @@
+﻿using HutongGames.PlayMaker;
+using BlueOrb.Common.Container;
+using BlueOrb.Messaging;
+
+namespace BlueOrb.Scripts.AI.Playmaker
+{
+    [ActionCategory("BlueOrb.UI")]
+    [HutongGames.PlayMaker.Tooltip("Alert")]
+    public class Alert : BasePlayMakerAction
+    {
+        [RequiredField]
+        public FsmOwnerDefault gameObject;
+
+        [RequiredField]
+        public FsmString AlertMessage;
+
+        private IEntity entity;
+
+        public override void Reset()
+        {
+            gameObject = null;
+        }
+
+        public override void OnEnter()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+                return;
+            }
+            this.entity = base.GetEntityBase(go);
+            MessageDispatcher.Instance.DispatchMsg("Alert", 0f, null, "Hud Controller", AlertMessage.Value);
+            Finish();
+        }
+    }
+}
