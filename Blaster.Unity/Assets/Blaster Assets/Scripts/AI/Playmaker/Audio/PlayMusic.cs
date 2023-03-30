4b19c195c912c96c33eb3ce3f0afbd3d0863bb7b
Brian Karcher <bkarcher@colony-games.com>
2023-03-30 00:56:54 -0700

    Music playing updates.

---
+++ Blaster.Unity/Assets/Blaster Assets/Scripts/AI/Playmaker/Audio/PlayMusic.cs
@@ -0,0 +1,21 @@
+﻿using HutongGames.PlayMaker;
+using UnityEngine;
+using BlueOrb.Base.Manager;
+using TooltipAttribute = HutongGames.PlayMaker.TooltipAttribute;
+
+namespace BlueOrb.Scripts.AI.Playmaker
+{
+    [ActionCategory("BlueOrb.Audio")]
+    [Tooltip("Play music on the music track")]
+    public class PlayMusic : FsmStateAction
+    {
+        public AudioClip AudioClip;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            GameStateController.Instance.PlayMusic(AudioClip);
+            Finish();
+        }
+    }
+}
